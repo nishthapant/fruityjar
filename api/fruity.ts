b01b2e56857c66ea5efd2aa8c7cb5f7116b9bbf9
Nishtha Pant <nishthapant@Nishthas-MacBook-Pro.local>
2025-07-03 09:23:13 -0600

    chore: updated header in proxy

--- api/fruity.ts
+++ api/fruity.ts
@@ -1,7 +1,17 @@
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, x-api-key");
+
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+
   try {
     const response = await fetch("https://fruity-proxy.vercel.app/api/fruits", {
-      headers: { "x-api-key": "fruit-api-challenge-2025" },
+      headers: {
+        'x-api-key': 'fruit-api-challenge-2025'
+      }
     });
 
     if (!response.ok) {
